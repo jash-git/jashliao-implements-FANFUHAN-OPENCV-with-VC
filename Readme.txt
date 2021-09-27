f3d523903f786d5304cbf203103f46644ae71474
Jash.Liao <jash.liao@gmail.com>
2021-09-27 15:47:50 +0800

    Add 202109271547

--- Readme.txt
+++ Readme.txt
@@ -9,7 +9,9 @@
 
 	jashliao 用 VC++ 實現 fanfuhan OpenCV 教學002 ~ opencv-002-色彩空間轉換（cvtcolor）與圖像保存（imwrite）
 		http://jashliao.eu/wordpress/2020/02/07/fanfuhan-opencv-%e6%95%99%e5%ad%b8002-opencv-002-%e8%89%b2%e5%bd%a9%e7%a9%ba%e9%96%93%e8%bd%89%e6%8f%9b%ef%bc%88cvtcolor%ef%bc%89%e8%88%87%e5%9c%96%e5%83%8f%e4%bf%9d%e5%ad%98%ef%bc%88imwrite/
-		
+	
+	jashliao 用 VC++ 實現 fanfuhan OpenCV 教學003 ~ opencv-003-圖像對象（Mat）創建與賦值 [圖片拷貝(複製)、建立空白畫布、建立運算濾波器(Filter)]
+		http://jashliao.eu/wordpress/2020/02/10/fanfuhan-opencv-%e6%95%99%e5%ad%b8003-opencv-003-%e5%9c%96%e5%83%8f%e5%b0%8d%e8%b1%a1%ef%bc%88mat%ef%bc%89%e5%89%b5%e5%bb%ba%e8%88%87%e8%b3%a6%e5%80%bc/
 		
 		
 		
---
+++ project_code/VC_FANFUHAN_OPENCV/VC_FANFUHAN_OPENCV003/VC_FANFUHAN_OPENCV003.cpp
@@ -0,0 +1,94 @@
+﻿// VC_FANFUHAN_OPENCV003.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+/*
+// Debug | x32
+通用屬性
+| C/C++
+|	| 一般
+|		| 其他 Include 目錄 -> C:\opencv\build\include
+|
+| 連結器
+| 	|一一般
+|		|  其他程式庫目錄 -> C:\opencv\build\x64\vc15\lib
+|
+| 	|一輸入
+|		| 其他相依性 -> opencv_world411d.lib;%(AdditionalDependencies)
+
+// Releas | x64
+組態屬性
+| C/C++
+|	| 一般
+|		| 其他 Include 目錄 -> C:\opencv\build\include
+|
+| 連結器
+| 	|一般
+|		| 其他程式庫目錄 -> C:\opencv\build\x64\vc15\lib
+|
+| 	|一輸入
+|		| 其他相依性 -> opencv_world411.lib;%(AdditionalDependencies)
+
+*/
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+void pause()
+{
+	printf("Press Enter key to continue...");
+	fgetc(stdin);
+}
+
+int main()
+{
+	Mat src = imread("../../images/l_hires.jpg");//IDE 測試路徑
+	//Mat src = imread("../../../images/l_hires.jpg");//執行檔 測試路徑
+
+	if (src.empty()) {
+		cout << "could not load image..." << endl;
+		pause();
+		return -1;
+	}
+	else
+	{
+		// 通过克隆或复制创建图像对象，m1和src指向不同内存块
+		Mat m1 = src.clone();
+		Mat m2;
+		src.copyTo(m2);
+
+		// 赋值法，m3和src指向同一内存块
+		Mat m3 = src;
+		namedWindow("input");
+		imshow("input", m3);
+
+		// 创建空白图像
+		Mat m4 = Mat::zeros(src.size(), src.type());
+		Mat m5 = Mat::zeros(Size(512, 512), CV_8UC3);
+		Mat m6 = Mat::ones(Size(512, 512), CV_8UC3);
+		Mat m7 = Mat(Size(512, 512), CV_8UC3,Scalar(255,255,255));//白色畫布
+		namedWindow("m4-input(zeros)");
+		imshow("m4-input(zeros)", m4);
+		namedWindow("m5-Size(512, 512)[zeros]");
+		imshow("m5-Size(512, 512)[zeros]", m5);
+		namedWindow("m6-Size(512, 512)[ones]");
+		imshow("m6-Size(512, 512)[ones]", m6);
+		namedWindow("m7-Size(512, 512)[Scalar(255,255,255)]");
+		imshow("m7-Size(512, 512)[Scalar(255,255,255)]", m7);
+
+		// kernel: [0, -1, 0
+		//          -1, 5, -1
+		//          0, -1, 0]
+		Mat kernel = (Mat_<char>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+
+		waitKey(0);
+	}
+
+	return 0;
+}
+
