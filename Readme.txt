6e3cd30754399c85848828d109273c62a337ceff
Jash.Liao <jash.liao@gmail.com>
2021-09-27 11:46:53 +0800

    Add 202109271146

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,8 @@
-﻿jashliao 用 VC 實現 FANFUHAN OPENCV教學範例
+﻿jashliao 用 VC++ 實現 FANFUHAN OPENCV教學範例
 
 資料來源: https://fanfuhan.github.io/
 
 
 自己對應部落格網址列表:
+	jashliao 用 VC++ 實現 fanfuhan OpenCV 教學001 ~ opencv-001-讀取（imread）、顯示（imshow）與儲存(imwrite)圖像
+		http://jashliao.eu/wordpress/2020/02/07/fanfuhan-opencv-%e6%95%99%e5%ad%b8001-opencv-001-%e8%ae%80%e5%8f%96%ef%bc%88imread%ef%bc%89%e8%88%87%e9%a1%af%e7%a4%ba%ef%bc%88imshow%ef%bc%89%e5%9c%96%e5%83%8f/
---
+++ project_code/VC_FANFUHAN_OPENCV/VC_FANFUHAN_OPENCV001/VC_FANFUHAN_OPENCV001.cpp
@@ -0,0 +1,66 @@
+// VC_FANFUHAN_OPENCV001.cpp : 定義主控台應用程式的進入點。
+//
+/*
+// Debug | x32
+通用屬性
+| C/C++
+|	| 一般
+|		| 其他 Include 目錄 -> C:\opencv\build\include
+|
+| 連結器
+| 	|一一般
+|		|  其他程式庫目錄 -> C:\opencv\build\x64\vc15\lib
+|
+| 	|一輸入
+|		| 其他相依性 -> opencv_world411d.lib;%(AdditionalDependencies)
+
+// Releas | x64
+組態屬性
+| C/C++
+|	| 一般
+|		| 其他 Include 目錄 -> C:\opencv\build\include
+|
+| 連結器
+| 	|一般
+|		| 其他程式庫目錄 -> C:\opencv\build\x64\vc15\lib
+|
+| 	|一輸入
+|		| 其他相依性 -> opencv_world411.lib;%(AdditionalDependencies)
+
+*/
+#include "stdafx.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+void pause()
+{
+	printf("Press Enter key to continue...");
+	fgetc(stdin);
+}
+
+int main()
+{
+	Mat image = imread("../../images/l_hires.jpg", IMREAD_GRAYSCALE);//IDE 測試路徑
+	//Mat image = imread("../../../images/l_hires.jpg", IMREAD_GRAYSCALE);//執行檔 測試路徑
+
+	if (image.empty()) {
+		cout << "could not load image..." << endl;
+		pause();
+		return -1;
+	}
+	else
+	{
+		namedWindow("input");
+		imshow("input", image);
+		imwrite("output.jpg", image);
+		waitKey(0);
+	}
+	
+    return 0;
+}
+
