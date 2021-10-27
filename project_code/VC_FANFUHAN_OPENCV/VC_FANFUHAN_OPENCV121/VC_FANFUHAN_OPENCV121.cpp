cc99e73e1922808eb4e94ef73f39067cd0bb2ba0
Jash.Liao <jash.liao@gmail.com>
2021-10-27 13:01:20 +0800

    modified 202110271301

--- project_code/VC_FANFUHAN_OPENCV/VC_FANFUHAN_OPENCV121/VC_FANFUHAN_OPENCV121.cpp
+++ project_code/VC_FANFUHAN_OPENCV/VC_FANFUHAN_OPENCV121/VC_FANFUHAN_OPENCV121.cpp
@@ -112,6 +112,79 @@ int main()
 		*/
 		stylization(src, result, 5, 0.6);//卡通画效果
 		imshow("Stylization", result);
+
+		//OilPaint_forloop
+		//https://github.com/housewing/OpenCV-Image-Processing/blob/master/OilPaint_forloop.cpp
+		Mat OilPaint = Mat(src.size(), CV_8UC3);
+		int heigh = src.rows;
+		int width = src.cols;
+		int BrushSize = 5;
+		int levels = 20;
+
+		int* CountIntensity = new int[levels];
+		int* RedAverage = new int[levels];
+		int* GreenAverage = new int[levels];
+		int* BlueAverage = new int[levels];
+		for (int y = BrushSize; y < (heigh - BrushSize); y++)
+		{
+			for (int x = BrushSize; x < (width - BrushSize); x++)
+			{
+				//cout << "y " << y << " x " << x << endl;
+				// initialize the array
+				for (int i = 0; i < levels; i++)
+				{
+					CountIntensity[i] = 0;
+					RedAverage[i] = 0;
+					GreenAverage[i] = 0;
+					BlueAverage[i] = 0;
+				}
+
+				for (int j = -BrushSize; j <= BrushSize; j++)
+				{
+					for (int i = -BrushSize; i <= BrushSize; i++)
+					{
+						//cout << "y+j " << y+j << " x+i " << x+i << endl;
+						double b = src.at<cv::Vec3b>(y + j, x + i)[0];
+						double g = src.at<cv::Vec3b>(y + j, x + i)[1];
+						double r = src.at<cv::Vec3b>(y + j, x + i)[2];
+						//cout << "y+j " << y+j << " x+i " << x+i << " " << b << " " << g << " " << r << endl;
+
+						int nCurIntensity = (((r + g + b) / 3.0) * levels) / 255;
+						//cout << "y+j " << y+j << " x+i " << x+i << " " << nCurIntensity << endl;
+
+						int tmp = nCurIntensity;
+						CountIntensity[tmp]++;
+
+						BlueAverage[tmp] = BlueAverage[tmp] + b;
+						GreenAverage[tmp] = GreenAverage[tmp] + g;
+						RedAverage[tmp] = RedAverage[tmp] + r;
+						//cout << "Average " << RedAverage[tmp] << endl;
+					}
+				}
+
+				int nCurMax = 0;
+				int nMaxIndex = 0;
+				for (int nI = 0; nI < levels; nI++)
+				{
+					if (CountIntensity[nI] > nCurMax)
+					{
+						nCurMax = CountIntensity[nI];
+						nMaxIndex = nI;
+					}
+				}
+				//cout << "CurMax " << nCurMax << " MaxIndex " << nMaxIndex << endl;
+
+				double tmpB = BlueAverage[nMaxIndex] / nCurMax;
+				double tmpG = GreenAverage[nMaxIndex] / nCurMax;
+				double tmpR = RedAverage[nMaxIndex] / nCurMax;
+				Vec3b pixelColor(tmpB, tmpG, tmpR);
+				OilPaint.at<Vec3b>(y, x) = pixelColor;
+				//dst.at<Vec3b>(y,x)[0] = BlueAverage[nMaxIndex] / nCurMax;
+				//dst.at<Vec3b>(y,x)[1] = GreenAverage[nMaxIndex] / nCurMax;
+				//dst.at<Vec3b>(y,x)[2] = RedAverage[nMaxIndex] / nCurMax;
+			}
+		}
+		imshow("OilPaint", OilPaint);
 		waitKey(0);
 	}
 	return 0;
